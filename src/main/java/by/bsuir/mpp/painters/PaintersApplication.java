ac7a7932c90948647868d25cddb52f3b0c8cea36
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2018-02-18 17:57:30 +0300

    Initial commit

---
+++ src/main/java/by/bsuir/mpp/painters/PaintersApplication.java
@@ -0,0 +1,12 @@
+package by.bsuir.mpp.painters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintersApplication.class, args);
+	}
+}
