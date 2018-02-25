3f915814a2b549684dcc3db6962b7b5ac90c30e2
dyadyasanya <sashagrod@mail.ru>
2018-02-25 19:53:06 +0300

    structure

--- src/main/java/by/bsuir/spp/painters/PaintersApplication.java
+++ src/main/java/by/bsuir/spp/painters/PaintersApplication.java
@@ -1,4 +1,4 @@
-package by.bsuir.mpp.painters;
+package by.bsuir.spp.painters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/by/bsuir/spp/painters/PaintersApplicationTests.java
+++ src/test/java/by/bsuir/spp/painters/PaintersApplicationTests.java
@@ -1,4 +1,4 @@
-package by.bsuir.mpp.painters;
+package by.bsuir.spp.painters;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
